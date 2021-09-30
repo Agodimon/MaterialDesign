c28d71e190cac19b7613d9c3f131b064bcedc599
dima <agodimon@mail.ru>
2021-09-30 22:22:55 +0500

    Добавил значки к заголовкам

--- app/src/main/java/com/bignerdranch/android/materialdesign/ui/api/ApiActivity.kt
+++ app/src/main/java/com/bignerdranch/android/materialdesign/ui/api/ApiActivity.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.materialdesign.ui.api
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.bignerdranch.android.materialdesign.R
 import kotlinx.android.synthetic.main.activity_api.*
 
@@ -11,5 +12,12 @@ class ApiActivity : AppCompatActivity() {
         setContentView(R.layout.activity_api)
         view_pager.adapter = ViewPagerAdapter(baseContext, supportFragmentManager)
         tab_layout.setupWithViewPager(view_pager)
+        setCustomTabs()
+    }
+
+    private fun setCustomTabs() {
+        tab_layout.getTabAt(0)?.setIcon(R.drawable.ic_earth)
+        tab_layout.getTabAt(1)?.setIcon(R.drawable.ic_mars)
+        tab_layout.getTabAt(2)?.setIcon(R.drawable.ic_weather)
     }
 }
