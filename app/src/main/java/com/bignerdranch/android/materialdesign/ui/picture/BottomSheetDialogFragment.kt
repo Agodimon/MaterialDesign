4c458eaef04def9936c659b9843a2bf54bcb2f23
dima <agodimon@mail.ru>
2021-10-05 22:59:52 +0500

    Выполнил практику по уроку №4. 1. Примените знания, полученные о ConstraintLayout, в своём проекте. 2. Если дизайн и архитектура вашего приложения позволяют, добавьте CollapsingToolbarLayout на какой-либо экран (или FAB) и добавьте для него анимацию сворачивания/разворачивания или какое-то кастомное поведение. Поэкспериментируйте с параметрами snap, enterAlways, collapse и другими. 3. Добавьте анимацию элементов в вашем приложении, где это уместно. Или придумайте новый вариант взаимодействия с приложением с помощью MotionLayout.

--- app/src/main/java/com/bignerdranch/android/materialdesign/ui/picture/BottomSheetDialogFragment.kt
+++ app/src/main/java/com/bignerdranch/android/materialdesign/ui/picture/BottomSheetDialogFragment.kt
@@ -1,11 +1,14 @@
 package com.bignerdranch.android.materialdesign.ui.picture
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.bignerdranch.android.materialdesign.R
+import com.bignerdranch.android.materialdesign.ui.api.ApiActivity
+import com.bignerdranch.android.materialdesign.ui.collapsingtoolbar.CollapsingToolbarActivity
 import com.bignerdranch.android.materialdesign.util.toast
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.bottom_navigation_layout.*
@@ -25,7 +28,7 @@ class BottomNavigationDrawerFragment : BottomSheetDialogFragment() {
         super.onViewCreated(view, savedInstanceState)
         navigation_view.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
-                R.id.navigation_one -> toast("1")
+                R.id.navigation_one -> startActivity(Intent(context, CollapsingToolbarActivity::class.java))
                 R.id.navigation_two -> toast("2")
             }
             true
