38a152f885d43396fe363aa37ac0959260e10c55
dima <agodimon@mail.ru>
2021-10-19 23:01:38 +0500

    ВЫПОЛНИЛ ПРАКТИКУ ПО УРОКУ №8. 1. Добавил Splash screen для моего проекта. 2. * Добавил анимацию на Splash screen.
    Стартовал SplashActivity, а затем из неё запустил основной экран MainActivity.
    Добавил анимацию на картинку rotation.

---
+++ app/src/main/java/com/bignerdranch/android/materialdesign/ui/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.bignerdranch.android.materialdesign.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.LinearInterpolator
+import com.bignerdranch.android.materialdesign.databinding.ActivitySplashBinding
+
+
+class SplashActivity : AppCompatActivity() {
+    lateinit var handler: Handler
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageView.animate().rotationBy(1000f).setInterpolator(LinearInterpolator()).duration = 3000
+        handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
+    }
+}
